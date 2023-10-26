01fce7234f0a843d0119fd5bf378afa6c2166a7e
wuxh <wxh1220@gmail.com>
2023-10-26 21:34:59 +0800

    chore: add pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import Pages from 'vite-plugin-pages';
 import pkg from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Pages({
+      resolver: 'react',
+    }),
+  ],
   base: process.env.GITHUB_ACTIONS ? `/${pkg.name}/` : '/',
 })
